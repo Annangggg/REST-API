2d2fae1ae0efc760aa8bd6aa2c6b13961624fe38
Annangggg <dnnguyen000@gmail.com>
2022-08-25 12:10:28 +0200

    book.id should be created automatically

--- src/controllers/books.ts
+++ src/controllers/books.ts
@@ -2,10 +2,11 @@
 /** source/controllers/posts.ts */
 import { Request, Response, NextFunction } from 'express';
 
-let books: { id: string, title: string, type: string }[] = [
-    {"id": "0",  "title": "Heroes", "type": "Novel" },
-    { "id": "1", "title": "Bird", "type": "Newpaper" },
+let books: { title: string, type: string, id: number, }[] = [
+    { "title": "Heroes", "type": "Novel","id": 1 },
+    { "title": "Bird", "type": "Newpaper", "id": 2 },
 ];
+
 
 
 // getting all books
@@ -15,33 +16,46 @@ const getBooks = ((req: Request, res: Response) => {
 });
 
 // adding a book
-const addBook = async (req: Request, res: Response) => {
+const addBook = async (req: Request, res: Response, next:NextFunction) => {
     // get the data from req.body
     let book = req.body;
+    let id = 1;
+    
     //let id = req.params.body;
-    books.push(book);
+    
+    books.forEach((item) => {
+        if (item.id >= id) 
+        {
+            id = item.id +1;
+        }
+        else {
+            next();
+        }
+      });
+      book.id = id;
+      books.push(book);
     res.status(201).json(book);
     
 }
 
 const deleteBook = async (req: Request, res: Response) => {
     // get the post id from req.params and delete
-    books = books.filter(({ id }) => id !== req.params.id);
+    books = books.filter(({ id }) => id !== parseInt(req.params.id));
     // return list of books 
-    return res.status(204).json(books);
+    return res.status(200).json(books);
 };
  
 // updating a post
 const updateBook = async (req: Request, res: Response, next: NextFunction) => {
     // get the book id from the req.params
-    const getBook = books.find(i => i.id === req.params.id);
+    const getBook = books.find(i => i.id === parseInt(req.params.id));
 
     if(!getBook) return res.status(404).json({})
     else {
         console.log(getBook);
         getBook.title = req.body.title;
         getBook.type = req.body.type;
-        getBook.id = req.params.id;
+        getBook.id = parseInt(req.params.id);
         res.json(getBook);
     }
 }
